5e17056c1ba00c68a1dedf9ef4ec283ac68e16c3
23IT101 POPAT HEMANGI <152361000+23it101PopatHemangi@users.noreply.github.com>
2025-03-13 03:54:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <u><h1>Simple Counter App</h1></u>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
